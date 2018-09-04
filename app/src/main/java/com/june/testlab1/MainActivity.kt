b390857b91866a3c72e060520e6d0e22294b4352
Jz <bringtojz@gmail.com>
2018-09-05 01:17:46 +0700

    update facebook

--- app/src/main/java/com/june/testlab1/MainActivity.kt
+++ app/src/main/java/com/june/testlab1/MainActivity.kt
@@ -3,22 +3,29 @@ package com.june.testlab1
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.facebook.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.auth.FirebaseAuth
 
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    var callbackManager : CallbackManager? = null
     var mAuth: FirebaseAuth? = null
     private val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         mAuth = FirebaseAuth.getInstance()
 
@@ -32,8 +39,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
         }
 
+        txvRegister.setOnClickListener{
+            startActivity(Intent(this@MainActivity,RegisterActivity::class.java))
+        }
+
+        callbackManager = CallbackManager.Factory.create()
+        btnLoginWithFacebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult?) {
+                txvResultLogin.text = "Login Success $(result?.accessToken?.userId)" + "$(result?.accessToken?.token)"
+            }
+
+            override fun onCancel() {
+                Log.d(TAG,"Facebook Cancel")
+            }
+
+            override fun onError(error: FacebookException?) {
+
+            }
+
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
     }
 }
+
+
 
 
 
---
+++ app/src/main/java/com/june/testlab1/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.june.testlab1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+
+class MyApplication : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_application)
+
+    }
+}
