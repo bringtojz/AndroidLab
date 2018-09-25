4c4e1b1e2e397e6efe4a6b3fc1f3462d65880f2f
Jz <bringtojz@gmail.com>
2018-09-25 16:54:06 +0700

    update logo app

--- app/src/main/java/com/june/testlab1/ui/Page2Fragment.kt
+++ app/src/main/java/com/june/testlab1/ui/Page2Fragment.kt
@@ -4,22 +4,30 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.design.R.id.container
 import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.june.testlab1.BakeryObject
+import android.widget.ImageView
 import com.june.testlab1.R
 import com.june.testlab1.networking.APIModule
 import com.june.testlab1.networking.modelAPI.starwar.ResultsItem
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_page2.*
-import java.util.zip.Inflater
+import android.support.design.widget.TabLayout
+import android.widget.TextView
+import android.R.attr.fragment
+
+
+
+
+
+
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -29,6 +37,7 @@ class Page2Fragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
+
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
@@ -53,40 +62,22 @@ class Page2Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        var myBasket: ArrayList<BakeryObject.Bakery> = arrayListOf()
-//
-//        var item1 : BakeryObject.Bakery = BakeryObject.Bakery("Cake","XXXXXX","https://pbs.twimg.com/profile_images/604644048/sign051.gif")
-//        var item2 : BakeryObject.Bakery = BakeryObject.Bakery("Pie","XXXXXX1","http://www.terameracake.com/wp-content/uploads/2015/12/2-Tier-Special-Cake-2.jpg")
-//        var item3 : BakeryObject.Bakery = BakeryObject.Bakery("Icesceam","XXXXXX2","http://www.terameracake.com/wp-content/uploads/2015/12/2-Tier-Special-Cake-2.jpg")
-//        var item4 : BakeryObject.Bakery = BakeryObject.Bakery("Toast","XXXXXX3","http://www.terameracake.com/wp-content/uploads/2015/12/2-Tier-Special-Cake-2.jpg")
-//        var item5 : BakeryObject.Bakery = BakeryObject.Bakery("Bingsu","XXXXXX4","http://www.terameracake.com/wp-content/uploads/2015/12/2-Tier-Special-Cake-2.jpg")
-//
-//        myBasket.add(item1)
-//        myBasket.add(item2)
-//        myBasket.add(item3)
-//        myBasket.add(item4)
-//        myBasket.add(item5)
-
-
-
         btnPassTo21.setOnClickListener {
             var intent:Intent = Intent(activity,Page2_1Activity::class.java)
             startActivity(intent)
         }
 
 
-
-
         APIModule.starwarconnect().getstarwar()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        {
+                        { Log.e("Status", "On Next")
                             viewAdapter = MyAdapter(it.results as List<ResultsItem>)
                                 recyclerView.setHasFixedSize(true)
                                 recyclerView.layoutManager = viewManager
                                 recyclerView.adapter = viewAdapter
-
+                                val nameView = view.findViewById(R.id.recycleview) as TextView
 
                         },
                         //On Error
@@ -96,6 +87,8 @@ class Page2Fragment : Fragment() {
                 )
 
     }
+
+
 
     fun onButtonPressed(uri: Uri) {
         listener?.onFragmentInteraction(uri)
