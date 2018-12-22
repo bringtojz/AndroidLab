1101695597f46b0c907571f9e494447061c99a9c
Jz <bringtojz@gmail.com>
2018-12-22 12:45:30 +0700

    ีupdate Register API

--- app/src/main/java/com/june/testlab1/RegisterAPIActivity.kt
+++ app/src/main/java/com/june/testlab1/RegisterAPIActivity.kt
@@ -8,12 +8,18 @@ import com.june.testlab1.networking.modelAPI.user.RegisterApiReq
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_register_api.*
+import org.jetbrains.anko.indeterminateProgressDialog
+import org.jetbrains.anko.toast
 
 class RegisterAPIActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_api)
+
+        val mProgressDialog = indeterminateProgressDialog("กำลังลงทะเบียน")
+        val ShowBranchError = "รหัสสาขาผิด"
+
 
         btnRegister.setOnClickListener {
             var registerApiReq = RegisterApiReq (edtUserName.text.toString(),edtUserId.text.toString(),edtPassword.text.toString())
@@ -22,11 +28,18 @@ class RegisterAPIActivity : AppCompatActivity() {
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
                             //On Next  200 OK
-                            { Log.e("Status", "On Next") },
+                            { Log.e("Status", "On Next")
+                                toast("ลงทะเบียนเรียบร้อย")
+                                mProgressDialog.dismiss()
+                            },
                             //On Error
                             { Log.e("Status", "On Error") },
                             //On Complete
-                            { Log.e("Status", "On Complete") }
+                            { Log.e("Status", "On Complete")
+                                edtUserName.text = null
+                                edtPassword.text = null
+                                edtUserId.text = null
+                            }
                     )
         }
     }
